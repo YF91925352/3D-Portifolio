af12fc1b3f3202a35ed8c8762385cd51a1a46e0e
YF91925352 <zyf91925352@outlook.com>
2023-11-24 17:25:24 +0100

    add first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo } from "../assets";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
+          <span className=" text-white text-[18px]  cursor-pointer hidden sm:block  ">
+            JavaScript Mastery
+          </span>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => {
+                setActive(link.title);
+              }}
+            >
+              {/* it needs to change to router and use navigate later */}
+              <a href={`${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
