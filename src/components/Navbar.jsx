b0a239100cf398f8f7e142badeb25cf34b4aebc8
YF91925352 <zyf91925352@outlook.com>
2023-11-25 14:01:02 +0100

    add navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,75 @@
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo } from "../assets";
+import { logo, menu, close } from "../assets";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 const Navbar = () => {
   const [active, setActive] = useState("");
-
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex justify-between items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
-          to="/"
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
+      <Link
+        to="/"
+        className="flex items-center gap-2"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}
+      >
+        <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
+        <span className=" text-white text-[18px]  cursor-pointer hidden sm:block  ">
+          JavaScript Mastery
+        </span>
+      </Link>
+      <ul className="list-none hidden sm:flex gap-10">
+        {navLinks.map((link) => (
+          <li
+            key={link.id}
+            className={`${
+              active === link.title ? "text-white" : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => {
+              setActive(link.title);
+            }}
+          >
+            {/* it needs to change to router and use navigate later */}
+            <a href={`${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden ">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28] object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+        />
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
         >
-          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
-          <span className=" text-white text-[18px]  cursor-pointer hidden sm:block  ">
-            JavaScript Mastery
-          </span>
-        </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => {
-                setActive(link.title);
-              }}
-            >
-              {/* it needs to change to router and use navigate later */}
-              <a href={`${link.id}`}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
+          <ul className="list-none flex items-center flex-col gap-4">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                }font-poppins text-[16px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                }}
+              >
+                {/* it needs to change to router and use navigate later */}
+                <a href={`${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
